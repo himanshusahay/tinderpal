95c47b9c26e2d45a3ee94b34c900a45f515f03c6
Myles Spencer <mespencer@wpi.edu>
2017-01-14 00:46:22 -0500

    Basic Facebook authentication

--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.core.urlresolvers import reverse
 
 def index(request):
 	return render(request, 'app/index.html')
+
+def login(request):
+	return HttpResponse("Success")
+
+def landing(request):
+	return render(request, 'app/landing.html')
