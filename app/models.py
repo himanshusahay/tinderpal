5cf4acfc9ed8faa38ba4e0bc143a8c19b787c739
Himanshu Sahay <himanshusahay@github.com>
2017-01-13 19:15:56 -0800

    Added model for lines and set AppConfig in INSTALLED_APPS

--- app/models.py
+++ app/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Line(models.Model):
+	line_id = models.BigIntegerField()
+	line_text = models.TextField()
+	tags = models.ForeignKey(Tag)
+	categories = models.ForeignKey(Category)
+
+	def __str__(self):
+		return self.line_text + "\n"
+
+class Tag(models.Model):
+	tag_name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.tag_name + "\n"
+
+class Category(models.Model):
+	category_name = models.CharField(max_length = 50)
+
+	def __str__(self):
+		return self.category_name + "\n"
+
