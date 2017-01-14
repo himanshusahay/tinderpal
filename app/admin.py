ec0ee9c426f9afe0b1cfa9da5eff65c05c88b277
Himanshu Sahay <himanshusahay@github.com>
2017-01-13 20:02:34 -0800

    Modified models and added admin interface

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Category, Line
+
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Line)
--- app/models.py
+++ app/models.py
@@ -2,20 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Line(models.Model):
-	line_id = models.BigIntegerField()
-	line_text = models.TextField()
-	tags = models.ForeignKey(Tag)
-	categories = models.ForeignKey(Category)
-
-	def __str__(self):
-		return self.line_text + "\n"
-
 class Tag(models.Model):
 	tag_name = models.CharField(max_length = 100)
 
 	def __str__(self):
 		return self.tag_name + "\n"
+
+	class Meta:
+		ordering = ('tag_name', )
 
 class Category(models.Model):
 	category_name = models.CharField(max_length = 50)
@@ -23,3 +17,17 @@ class Category(models.Model):
 	def __str__(self):
 		return self.category_name + "\n"
 
+	class Meta:
+		ordering = ('category_name', )
+		verbose_name_plural = "categories"
+
+class Line(models.Model):
+	line_text = models.TextField()
+	tags = models.ManyToManyField(Tag)
+	categories = models.ManyToManyField(Category)
+
+	def __str__(self):
+		return self.tag + "\n"
+
+	class Meta:
+		ordering = ('line_text', )
