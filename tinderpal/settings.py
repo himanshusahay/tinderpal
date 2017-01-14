04d9afd4a3dfcf7b6b4a5dab68b9d763989847e8
Himanshu Sahay <himanshusahay@github.com>
2017-01-13 21:50:51 -0800

    using cookie-based sessions

--- tinderpal/settings.py
+++ tinderpal/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Using cookie-based sessions
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+SESSION_COOKIE_HTTPONLY = True
